10b8e28ae4e3f802392d0bd18b9b4c280b2d354b
sarahlilianjohnson <sarahlilianjohnson@gmail.com>
2023-06-13 21:41:13 -0400

    Ajout d'un libraire

---
+++ SDK_Laboratoire1/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,39 @@
+﻿namespace LibrairieConversionNote;
+    public static class ConversionAlphabetiqueToNote
+{
+    public static string Transform(string note)
+    {
+        if (note == "A")
+        {
+            return "LA";
+        }
+        else if (note == "B")
+        {
+            return "SI";
+        }
+        else if (note == "C")
+        {
+            return "DO";
+        }
+        else if (note == "D")
+        {
+            return "RE";
+        }
+        else if (note == "E")
+        {
+            return "MI";
+        }
+        else if (note == "F")
+        {
+            return "FA";
+        }
+        else if (note == "G")
+        {
+            return "SOL";
+        }
+        else
+        {
+            return "ERREUR! CECE N'EST PAS UNE NOTE!";
+        }
+    }
+}
